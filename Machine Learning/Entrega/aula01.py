468ab88d1a7152c53757b7bfdf1bbd9cb8799362
FernandoAcacioSchatz <fernandoacacioschatz@gmail.com>
2026-06-22 09:51:16 -0300

    cria arquivo Entrega

---
+++ Machine Learning/Entrega/aula01.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from sklearn.tree import DecisionTreeClassifier
+
+dados = {
+    "horasEstudos": [1, 2, 3, 4, 5, 6],
+    "aprovado": [0, 0, 0, 1, 1, 1],
+}
+
+# Cria a tabela
+df = pd.DataFrame(dados)
+
+# Entradas
+x = df[["horasEstudos"]]
+
+# Saídas
+y = df["aprovado"]
+
+# Cria o modelo
+modelo = DecisionTreeClassifier()
+
+# Treina o modelo
+modelo.fit(x, y)
+
+# Nova pessoa
+nova_pessoa = [[7]]
+
+# Faz previsão
+resultado = modelo.predict(nova_pessoa)
+
+print(resultado)
